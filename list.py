d21bc8b5cc2e6de807d70a252f4a2f141d0e4e4c
Åsa Jonsson <asaj71@gmail.com>
2022-02-17 08:58:42 +0100

    falsy and list

---
+++ list.py
@@ -0,0 +1,8 @@
+users=['Ed', 'Asa', 'Jonas', 'Snow', 'Misse']
+
+print(users[0])
+
+
+alot_zeros = [0] * 100
+
+print(alot_zeros)
--- statements.py
+++ statements.py
@@ -9,7 +9,25 @@
 # else:  
   # print(" sorry too young") 
 
-age = 18
+#age = 18
 
-if age != 24:
-  print(" hey you not 24")
+#if age != 24:
+  #print(" hey you not 24")
+
+#is_logged = True
+#is_logged: bool
+#if is_logged:
+   # print('welcome user')
+
+
+is_logged = True
+
+if 1:
+    print('welcome user')
+  
+  
+# Falsy values
+# 0
+# ""
+# []
+
